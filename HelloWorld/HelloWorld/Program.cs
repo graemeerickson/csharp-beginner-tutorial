0957cfc1bafd09f0878f5e5e4fe1ed841b9374f0
graemeerickson <graemeerickson@gmail.com>
2018-10-15 14:41:51 -0700

    More type casting practice, from string to boolean

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -8,14 +8,14 @@ namespace HelloWorld
         {
             try
             {
-                var number = "1234";
-                int b = Convert.ToByte(number);
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
 
                 Console.WriteLine(b);
             }
             catch (Exception)
             {
-                Console.WriteLine("The number could not be converted to a byte.");
+                Console.WriteLine("The string could not be converted to a boolean.");
             }
         }
     }
