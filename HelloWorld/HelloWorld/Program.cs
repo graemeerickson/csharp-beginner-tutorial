a2eca9374e45550cd69ae112a42d13d15ae1aa53
graemeerickson <graemeerickson@gmail.com>
2018-10-16 16:21:04 -0700

    Initializing arrays, understanding default values by data type.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using HelloWorld.Math;
 
 namespace HelloWorld
 {
@@ -7,14 +6,21 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            var graeme = new Person();
-            graeme.FirstName = "Graeme";
-            graeme.LastName = "Erickson";
-            graeme.Introduce();
+            var numbers = new int[3];
+            numbers[0] = 1;
 
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(1, 2);
-            Console.WriteLine("Result: " + result);
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "Jack", "John", "Mary" };
         }
     }
 }
