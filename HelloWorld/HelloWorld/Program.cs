f7ccaf84b733b1edd41df2a51b7911bc386ba882
graemeerickson <graemeerickson@gmail.com>
2018-10-18 09:44:29 -0700

    Switch statement practice

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,21 +1,29 @@
-﻿using System;
-
-namespace HelloWorld
+﻿namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            bool isGoldCustomer = true;
+            var season = Season.Autumn;
 
-            //float price;
-            //if (isGoldCustomer)
-            //    price = 19.95f;
-            //else
-                //price = 29.95f;
-
-            float price = (isGoldCustomer) ? 19.95f : 29.95f;
-            Console.WriteLine("Price: " + price);
+            switch (season)
+            {
+                case Season.Spring:
+                    System.Console.WriteLine("It's spring.");
+                    break;
+                case Season.Summer:
+                    System.Console.WriteLine("It's summer.");
+                    break;
+                case Season.Autumn:
+                    System.Console.WriteLine("It's autumn.");
+                    break;
+                case Season.Winter:
+                    System.Console.WriteLine("It's winter.");
+                    break;
+                default:
+                    System.Console.WriteLine("I don't understand that season.");
+                    break;
+            }
         }
     }
 }
