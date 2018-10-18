6c7aedba0ca91bc1b7cf5a3962e18361715f9b8d
graemeerickson <graemeerickson@gmail.com>
2018-10-18 11:31:09 -0700

    Foreach loop practice with a string

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -12,17 +12,13 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            for (var i = 1; i <= 10; i++)
-            {
-                if (i % 2 == 0)
-                    Console.WriteLine(i);
-            }
+            var name = "Graeme Erickson";
 
-            for (var i = 10; i >= 1; i--)
-            {
-                if (i % 2 == 0)
-                    Console.WriteLine(i);
-            }
+            //for (var i = 0; i < name.Length; i++)
+                //Console.WriteLine(name[i]);
+
+            foreach (var character in name)
+                Console.WriteLine(character);
         }
     }
 }
