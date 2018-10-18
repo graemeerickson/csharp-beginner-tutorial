e764659904935eb097362d855f319c573515d824
graemeerickson <graemeerickson@gmail.com>
2018-10-18 11:33:35 -0700

    Foreach loop practice with a string & array

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -13,12 +13,16 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             var name = "Graeme Erickson";
+            var numbers = new int[] { 1, 2, 3, 4 };
 
             //for (var i = 0; i < name.Length; i++)
                 //Console.WriteLine(name[i]);
 
             foreach (var character in name)
                 Console.WriteLine(character);
+
+            foreach (var number in numbers)
+                Console.WriteLine(number);
         }
     }
 }
