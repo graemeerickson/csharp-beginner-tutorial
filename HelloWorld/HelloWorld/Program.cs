149cfbf55bb263598f72df722efe9be973be0719
graemeerickson <graemeerickson@gmail.com>
2018-10-18 11:04:46 -0700

    Receive speed limit and car speed from user, then calculate number of demerit points

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -12,14 +12,25 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter image width: ");
-            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter speed limit: ");
+            int speedLimit = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter image height: ");
-            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter speed of the car: ");
+            int carSpeed = Convert.ToInt32(Console.ReadLine());
 
-            var orientation = width > height ? ImageOrientation.Landscape : ImageOrientation.Portrait;
-            Console.WriteLine("Image orientation: " + orientation);
+            bool isSpeeding = carSpeed > speedLimit;
+
+            if (!isSpeeding)
+                Console.WriteLine("Ok");
+            else
+            {
+                const int kmPerDemeritPoint = 5;
+                var demeritPoints = (carSpeed - speedLimit) / kmPerDemeritPoint;
+                if (demeritPoints > 12)
+                    Console.WriteLine("License suspended");
+                else
+                    Console.WriteLine("Demerit points: " + demeritPoints);
+            }
         }
     }
 }
